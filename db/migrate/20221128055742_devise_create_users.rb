43c849e6b22a752268b862c17f9b4c547117a56f
Janaka Vithanage <jkvithana@gmail.com>
2022-11-28 20:07:25 +1100

    User model created with devise authentication.

--- db/migrate/20221128055742_devise_create_users.rb
+++ db/migrate/20221128055742_devise_create_users.rb
@@ -6,6 +6,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :username
+      t.string :first_name
+      t.string :last_name
 
       ## Recoverable
       t.string   :reset_password_token
