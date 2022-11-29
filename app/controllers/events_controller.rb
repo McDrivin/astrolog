8f8523ab6126a0a9963a814b0fb1fec214dafc4a
Janaka Vithanage <jkvithana@gmail.com>
2022-11-30 10:59:41 +1100

    actions added for all models.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,11 @@
 class EventsController < ApplicationController
+  skip_before_action :authenticate_user!
+
+  def index
+    @events = Event.all
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
 end
--- app/controllers/launches_controller.rb
+++ app/controllers/launches_controller.rb
@@ -1,2 +1,11 @@
 class LaunchesController < ApplicationController
+  skip_before_action :authenticate_user!
+
+  def index
+    @launches = Launch.all
+  end
+
+  def show
+    @launch = Launch.find(params[:id])
+  end
 end
