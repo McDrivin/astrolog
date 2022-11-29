92bc245a943cbda577991a528e7a9ba39288ea41
Janaka Vithanage <jkvithana@gmail.com>
2022-11-29 15:56:35 +1100

    Launch model created.

---
+++ app/models/launch.rb
@@ -0,0 +1,2 @@
+class Launch < ApplicationRecord
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  get 'launches', to: 'pages#launches'
-  get 'events', to: 'pages#events'
-  get 'agencies', to: 'pages#agencies'
-  get 'astronauts', to: 'pages#astronauts'
+
+  # resouces :events, only: [:index, :show]
 end
