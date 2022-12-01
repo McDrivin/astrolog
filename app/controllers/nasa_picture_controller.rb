7e419cbec9b892c9f97393a4baa21371ee061ec8
Jasdeep Singh <j.singhhh917@gmail.com>
2022-12-01 17:33:24 +1100

    people in space

--- app/controllers/nasa_picture_controller.rb
+++ app/controllers/nasa_picture_controller.rb
@@ -2,5 +2,7 @@ class NasaPictureController < ApplicationController
   def index
     @nasa_info = NasaApi.get_api_info
     @media_type_is_video = @nasa_info['media_type'].eql?('video')
+    # @in_space = NasaApi.ppl_in_space_api
+    @in_space = NasaApi.ppl_in_space_api
   end
 end
