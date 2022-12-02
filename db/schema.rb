b333ec7d27a8e7c1d50bd7e7e15cab023c4d94fd
Stephen Taylor <steponitph@gmail.com>
2022-12-02 12:34:36 +1100

    just checking to see what the schema's doing

--- db/schema.rb
+++ db/schema.rb
@@ -158,9 +158,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_01_110829) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "username"
-    t.string "first_name"
-    t.string "last_name"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
