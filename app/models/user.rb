e208c26ae93fdd2ac7eb1b272e970df89d2be94a
Janaka Vithanage <jkvithana@gmail.com>
2022-12-04 11:00:43 +1100

    login/signup pages improved.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,9 @@ class User < ApplicationRecord
   has_many :messages, dependent: :destroy
   has_many :topic_members, dependent: :destroy
 
-  # validates :username, uniqueness: true, presence: true
+  validates :username, uniqueness: true, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
