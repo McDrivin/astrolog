2556f7d8f036ff31996488f1d582d221a0c2644d
Stephen Taylor <steponitph@gmail.com>
2022-12-05 15:39:57 +1100

    please work.....

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,9 +2,3 @@ require "json"
 
 class Article < ApplicationRecord
 end
-
-# def self.article_api
-#   url = "https://api.spaceflightnewsapi.net/v3/articles"
-#   request_to_article_api = Net::HTTP.get(URI(url))
-#   JSON.parse request_to_article_api
-# end
