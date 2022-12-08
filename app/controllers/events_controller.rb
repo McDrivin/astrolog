c5412497d15806ce2054e6e9e4ea505659c5ce2c
Janaka Vithanage <jkvithana@gmail.com>
2022-12-09 10:19:49 +1100

    Event show page layout changed.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -15,5 +15,12 @@ class EventsController < ApplicationController
     video_url = @event.video_url
     @embed_video = "https://www.youtube.com/embed/#{video_url.split("v=").last}"
     @embed_id = video_url.split("v=").last
+    if @event.location.nil?
+      results = Geocoder.search("Pacific Ocean")
+    else
+      results = Geocoder.search(@event.location)
+    end
+    @lat = results[0].data["lat"]
+    @lng = results[0].data["lon"]
   end
 end
